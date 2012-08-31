d56d71eaad6847590a37e7ec3333aadc757a06d3
JR Boyens <jboyens@fooninja.org>
2012-08-31 00:13:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Passfinder::Application.config.secret_token = 'a643e64c67167dd7563bb2d8b24d4d7cb8b52dbac253423e17486b062f9a702e7b6990ccab35496e054ae26b0e46c3c113519a5161ef853dc44ed93976751cbf'
